91fbed4a956dd870f7735dfa5cac56c5d4cb78c6
xo28122000 <jainam.2000@yahoo.com>
2019-12-19 23:20:28 -0800

    checkpoint one complete

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by J on 12/18/19.
+//  Copyright © 2019 Jainam Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
