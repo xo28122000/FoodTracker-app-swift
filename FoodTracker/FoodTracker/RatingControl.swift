78ea6e7c89f4d53c59af962f1414ba9a9c0f5ba9
xo28122000 <jainam.2000@yahoo.com>
2019-12-23 23:33:41 -0800

    implement a custon control started

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,27 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by J on 12/23/19.
+//  Copyright © 2019 Jainam Shah. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+  //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    //MARK: Private Methods
+    
+    
+    
+}
